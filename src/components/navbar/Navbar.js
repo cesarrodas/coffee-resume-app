3de62c0cfb0175548056553c893e930e71b485d9
cesar rodas <rodascesar03@yahoo.com>
2023-02-24 21:30:24 -0600

    updated hamburger menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,12 +6,13 @@ function Navbar({ toggleMenu, scrollTop }) {
   return (
     <div className="Navbar">
       <div class="Navbar-item">
-        {/* <ul class="Navbar-sandwich">
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul> */}
-        <button onClick={toggleMenu}>Menu</button>
+        <button className="Navbar-button" onClick={toggleMenu}>
+          <ul class="Navbar-sandwich">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </button>
       </div>
       <div class="Navbar-item">
         <a><img className={`Navbar-logo ${ scrollTop > 30 ? "Navbar-small-logo" : ""}`} src={coffeelogo}></img></a>
